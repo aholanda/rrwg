7c814d47449e73d18ba003208cba713a1b280424
Adriano de Jesus Holanda <aholanda@usp.br>
2020-02-11 15:51:49 -0300

    start parsing arguments

--- src/main.c
+++ src/main.c
@@ -1,10 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "rrwg.h"
 
+float version=0.1;
+
+/* Output file name in CSV-like format. */
+char dat_file_name[MAXFN];
+/* Input file name in Pajek-like format. */
+char net_file_name[MAXFN];
+/* Flags passed to the program. */
+char flags[128];
+
+static void init() {
+        int i;
+
+        for (i=0; i<128; i++)
+                flags[i] = FALSE;
+        flags['b'] = TRUE;
+}
+
+static void show_usage_and_exit(char **av) {
+        fprintf(stderr, "usage: %s [+v] infile.net\n", av[0]);
+
+        exit(EXIT_FAILURE);
+}
+
+static void show_banner(char **av) {
+        if (flags['b'])
+                fprintf(stderr, "* This is %s version %2.2f\n",
+                        av[0], version);
+}
+
+static void eval_arg(char *a, char on_off) {
+        Boolean state=FALSE;
+
+        if (on_off == '+')
+                state = TRUE;
+
+        while (*(++a) != '\0') {
+                switch (*a) {
+                case 'v':
+                        flags['v'] = state;
+                        break;
+                default:
+                        break;
+                }
+        }
+}
+
+static Boolean scan_args(int ac, char**av) {
+        Boolean ok=FALSE;
+        int i;
+        char *a;
+
+        for (i=ac-1; i>0; i--) {
+                a = av[i];
+                if (*a == '+' || *a == '-')
+                        eval_arg(a, *a);
+                else {
+                        strncpy(net_file_name, a, MAXFN);
+                        ok = TRUE;
+                }
+        }
+        return ok;
+}
+
 int main(int argc, char **argv) {
 	struct graph *g;
+        Boolean go_on = FALSE;
+
+        init();
+        go_on = scan_args(argc, argv);
+        if (!go_on)
+                show_usage_and_exit(argv);
+
+        show_banner(argv);
+        printf("%s\n", net_file_name);
 
 	g = graph_init();
 
@@ -15,7 +88,6 @@ int main(int argc, char **argv) {
 	walk(g);
 
 	plot(g);
-
 
 	graph_destroy(g);
 
