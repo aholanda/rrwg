84d887a8b4b932a55845f5f44a46d9fdd8064224
Adriano de Jesus Holanda <aholanda@usp.br>
2020-02-17 09:54:37 -0300

    fix all buffer problems

--- src/R.c
+++ src/R.c
@@ -9,45 +9,37 @@
 #define XLAB "time"
 #define YLAB "visits"
 
-static char buf[BUFLEN]; /* local buffer */
-#define FPRINTF(...) do {				\
-		snprintf(&buf[0], BUFLEN, __VA_ARGS__);	\
-		fprintf(file, "%s", buf);		\
-	} while(0)
-
 #define NSTYLES 3
 static char*colors[NSTYLES]= {"red", "black", "dark blue"};
 
-static FILE *file; /* File to write the commands to plot the curves. */
+extern FILE *R_file; /* File to write the commands to plot the curves. */
 extern char R_file_name[MAXFN]; /* Output file name where the commands to plot are written. */
 
 static void R_output_format(char*prefix) {
-        static char fn[MAXFN];
         char *fmt="pdf";
 
-        snprintf(fn, BUFLEN, "%s('%s.%s')", fmt, prefix, fmt);
-        FPRINTF("%s\n", fn);
+        fprintf(R_file, "%s('%s.%s')\n", fmt, prefix, fmt);
 }
 
 static void R_load_data(struct graph *g) {
-	FPRINTF("data <- read.table(\"%s%s\", header=FALSE, sep=\"%s\")\n",
+	fprintf(R_file, "data <- read.table(\"%s%s\", header=FALSE, sep=\"%s\")\n",
 		g->name, ".dat", DATA_SEP);
-	FPRINTF("xmax <- nrow(data) # xmax is the number of rows\n");
-        FPRINTF("par(mfrow=c(%d, 1)) # set the number of rows\n",
+	fprintf(R_file, "xmax <- nrow(data) # xmax is the number of rows\n");
+        fprintf(R_file, "par(mfrow=c(%d, 1)) # set the number of rows\n",
 		g->w);
 }
 
 static void R_plot(struct graph *g, int walker_idx) {
-        FPRINTF("x <- seq(1,xmax)\n");
-        FPRINTF("ymin <- min(data[,%d:%d]/x)\n", (walker_idx*g->n)+1, (walker_idx+1)*g->n);
-        FPRINTF("ymax <- max(data[,%d:%d]/x)\n", (walker_idx*g->n)+1, (walker_idx+1)*g->n);
-	FPRINTF("plot(data[,%d]/x, col=\"%s\", type=\"l\", lwd=2, xlab=\"%s\", ylab=\"%s\", \
+        fprintf(R_file, "x <- seq(1,xmax)\n");
+        fprintf(R_file, "ymin <- min(data[,%d:%d]/x)\n", (walker_idx*g->n)+1, (walker_idx+1)*g->n);
+        fprintf(R_file, "ymax <- max(data[,%d:%d]/x)\n", (walker_idx*g->n)+1, (walker_idx+1)*g->n);
+	fprintf(R_file, "plot(data[,%d]/x, col=\"%s\", type=\"l\", lwd=2, xlab=\"%s\", ylab=\"%s\", \
                        xlim=c(0,%d), ylim=c(ymin,ymax))\n",
 		(walker_idx * g->n) + 1, colors[0], "n", "%visits", g->maxsteps);
 }
 
 static void R_line(struct graph *g, int walker_idx, int vertex_idx) {
-	FPRINTF ("lines(data[,%d]/x, col=\"%s\", lwd=2)\n",
+	fprintf(R_file, "lines(data[,%d]/x, col=\"%s\", lwd=2)\n",
 		 (walker_idx * g->n) + (vertex_idx + 1), colors[vertex_idx % NSTYLES]);
 
 }
@@ -55,27 +47,27 @@ static void R_line(struct graph *g, int walker_idx, int vertex_idx) {
 static void R_legend(struct graph *g) {
         int i;
 
-        FPRINTF("legend(xmax-xmax/10, ymax-ymax/10, legend=c(");
+        fprintf(R_file, "legend(xmax-xmax/10, ymax-ymax/10, legend=c(");
         for (i=0; i<g->n; i++) {
-                FPRINTF("\"v%d\"", i+1);
+                fprintf(R_file, "\"v%d\"", i+1);
                 if (i != g->n-1)
-                        FPRINTF(",");
+                        fprintf(R_file, ",");
         }
 
-	FPRINTF("), col=c(");
+	fprintf(R_file, "), col=c(");
         for (i=0; i<g->n; i++) {
-                FPRINTF("\"%s\"", colors[i%NSTYLES]);
+                fprintf(R_file, "\"%s\"", colors[i%NSTYLES]);
                 if (i != g->n-1)
-                        FPRINTF(",");
+                        fprintf(R_file, ",");
         }
-        FPRINTF("), ncol=1, cex=0.8, lwd=2, bty=\"n\")\n");
+        fprintf(R_file, "), ncol=1, cex=0.8, lwd=2, bty=\"n\")\n");
 }
 
 /*
   Fill the buffer with commands to finish the plot.
 */
 static void R_end() {
-	FPRINTF(buf, BUFLEN, "dev.off() # Close the file\n");
+	fprintf(R_file, "dev.off() # Close the file\n");
 }
 
 void R_fwrite_script(struct graph *g) {
@@ -83,7 +75,7 @@ void R_fwrite_script(struct graph *g) {
 
 	strncpy(R_file_name, g->name, MAXFN);
 	append_suffix(R_file_name, ".R");
-	file = Fopen(R_file_name, "w");
+	R_file = Fopen(R_file_name, "w");
 
         R_output_format(g->name);
 	R_load_data(g);
@@ -95,6 +87,6 @@ void R_fwrite_script(struct graph *g) {
 	}
 	R_end();
 
-	Fclose(file);
+	Fclose(R_file);
 	fprintf(stderr, "* Wrote %s\n", R_file_name);
 }
