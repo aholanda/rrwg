54324f0a73101cd671cab72f2be5573dd2b1b134
Adriano de Jesus Holanda <aholanda@usp.br>
2020-02-28 14:53:09 -0300

    declare global variables used in main

---
+++ include/rrwg-main.h
@@ -0,0 +1,9 @@
+#ifndef RRWG_MAIN_H
+#define RRWG_MAIN_H
+
+/* Store the arguments to the program. */
+extern char flags[128];
+/* Traces of the walks are written in the log_file. */
+extern FILE *log_file;
+
+#endif /* !RRWG_MAIN_H */
