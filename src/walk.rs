ccaa455c5fe7b72cc3ff78a13beea3abd7fa6c80
Adriano J. Holanda <ajholanda@gmail.com>
2020-06-17 17:36:18 -0300

    create RRWG struct and give up the idea of attach attrs to Vertex

--- src/walk.rs
+++ src/walk.rs
@@ -1,4 +1,4 @@
-use huji::{Attribute, Vertex, VertexIndex};
+use huji::{Graph, VertexIndex};
 
 pub type VisitorIndex = usize;
 
@@ -19,4 +19,12 @@ impl Walker {
     pub fn go_to(&mut self, v: VertexIndex) {
         self.path.push(v);
     }
+}
+
+pub struct RRWG<'r> {
+    graph: Graph<'r>,
+    walkers: Vec<Walker>,
+    alpha: f64,
+    max_steps: usize,
+    function: fn(f64, f64, f64, f64) -> f64,
 }
