1940e52fc48c43c7266ff34a09726579b59e6fef
Adriano J. Holanda <ajholanda@gmail.com>
2020-06-18 18:54:29 -0300

    dont use toml as graph description

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod walk;
 
-use std::{env, fs};
-
-use toml::Value;
+use std::env;
 
 use huji::Graph;
-use crate::walk::{RRWG, Walker};
+use crate::walk::{RRWG};
 
 fn print_help_msg() {
     println!("usage:
@@ -18,25 +16,13 @@ match_args {{increase|decrease}} <integer>
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut rrwg: &RRWG;
+    let mut rrwg: RRWG;
 
     match args.len() {
         2 => {
             let fname = &args[1];
-            read_file(fname);
+            rrwg = RRWG::new(fname);
         },
         _ => print_help_msg(),
     }
-
-    let walker = Walker::new(0);
-    let graph = Graph::new();
-
-    println!("{:?}", walker);
-}
-
-fn read_file(filename: &String) {
-    let toml_content = fs::read_to_string(filename)
-        .expect("Could not open file");
-
-    let rrwg_info = toml_content.parse::<Value>().unwrap();
 }
