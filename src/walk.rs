c350526e7e0dfb3b0124c5af4d4b55e927c490dc
Adriano J. Holanda <ajholanda@gmail.com>
2020-06-18 19:02:04 -0300

    include huji::io::pajek to parse graph description

--- src/walk.rs
+++ src/walk.rs
@@ -1,4 +1,8 @@
+use std::fs::File;
+use std::io::prelude::*;
 use huji::{Graph, VertexIndex};
+
+use huji::io::pajek;
 
 pub type VisitorIndex = usize;
 
@@ -27,4 +31,32 @@ pub struct RRWG<'r> {
     alpha: f64,
     max_steps: usize,
     function: fn(f64, f64, f64, f64) -> f64,
+}
+
+impl RRWG<'_> {
+    pub fn new(filename: &str) -> Self {
+        let mut rrwg: RRWG;
+
+        let mut file = File::open(filename)
+            .expect("Unable to open file");
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents)
+            .expect("Unable to read file");
+
+        pajek::read(filename);
+
+        Self {
+            graph: Graph::new(),
+            walkers: vec![],
+            alpha: 0.1,
+            max_steps: 10,
+            function: Self::exp,
+        }
+    }
+
+    fn exp(x: f64, y: f64, alpha: f64, b: f64) -> f64 {
+        0.2
+    }
+
 }
