bef73699c300d9c03f64672bdfb9729c4b7a280a
Adriano J. Holanda <ajholanda@gmail.com>
2020-07-03 17:24:24 -0300

    split walk module here to walks package there

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-mod walk;
-
 use std::env;
 
-use huji::data::Graph;
-use huji::io::pajek::read as read_pajek;
-use crate::walk::{RRWG};
+use huji::{data::Graph,
+           io::{Parse,
+                pajek::Pajek}};
+use walks::{Map, Walks};
 
 fn print_help_msg() {
     println!("usage:
-match_args <string>
+match_args <pajek filename>
     Check whether given string is the answer.
 match_args {{increase|decrease}} <integer>
     Increase or decrease given integer by one."
@@ -17,13 +16,14 @@ match_args {{increase|decrease}} <integer>
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut rrwg: RRWG;
+    let rrwg: Walks;
 
     match args.len() {
         2 => {
             let fname = &args[1];
-            let mut graph = read_pajek(fname);
-            rrwg = RRWG::new(fname);
+            let graph: Graph = *<Pajek as Parse>::read(fname);
+            println!("{:?}", graph);
+            rrwg = Walks::new(Map::Graph(&graph));
         },
         _ => print_help_msg(),
     }
