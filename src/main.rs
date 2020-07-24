3080bfb309fcd8fe2fecae90ae1012c1d2ad35ce
Adriano J. Holanda <ajholanda@gmail.com>
2020-07-24 17:49:38 -0300

    using Walker instead of Walks struct

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use huji::{adj::Graph,
            io::{Parser,
                 pajek::Pajek}};
-use walks::{Map, Walks};
+use walks::{Location, LocationIndex, Map, Walker};
 
 fn print_help_msg() {
     println!("usage:
@@ -14,25 +14,44 @@ match_args {{increase|decrease}} <integer>
     );
 }
 
-struct Exp;
 
-impl WalkFunction for Exp {
-    fn walk(parms: Parameters) {
+fn sum_all_visits_in_the_neighborhood(cur_loc_idx: LocationIndex,
+                                      walker: &Walker) -> usize {
+    let sum: usize = 0;
+    let neighbors = Location::get_neighbors(walker.get_map(),
+                                            cur_loc_idx);
 
+    // Traverse the neighbors of loc_idx, summing the number of
+    // visits of some chosen walker. The sum must be equal for all
+    // walkers.
+    for i in neighbors {
+        // 0 could be any value of visitor index, pick one almost
+        // randomly.
+        sum += walker.get_visits(i);
     }
+    sum
 }
+
+fn walk() -> LocationIndex {
+    0
+}
+
+const NW: u8 = 2;
+const MAX: usize = 8;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let rrwg: Walks;
+    let walkers: Vec<Walker> = vec![];
 
     match args.len() {
         2 => {
             let fname = &args[1];
             let mut pjk = Pajek::new(fname);
-            let graph: &mut Graph = pjk.read();
+            let mut graph: Graph = pjk.read();
             println!("{:?}", graph);
-            rrwg = Walks::new(Map::Graph(&graph), 2);
+            for i in 0..NW {
+                walkers.push(Walker::new(&Map::Graph(graph), 1, MAX, walk));
+            }
         },
         _ => print_help_msg(),
     }
